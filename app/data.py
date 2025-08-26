613890eb0589dd11850b024d89ee958142b64fbc
Ella Prince <ellahorton90@gmail.com>
2025-08-26 18:42:59 -0500

    Final changes to data.py

--- app/data.py
+++ app/data.py
@@ -9,16 +9,18 @@ from pymongo import MongoClient
 
 
 class MonsterDatabase:
+    """A class to manage a MongoDB collection of Monster objects."""
 
     def __init__(self):
+        """Initialize the MongoDB client and connect to the monsters collection"""
         load_dotenv()
         db_url = getenv("DB_URL")
         self.client = MongoClient(db_url, tlsCAFile=where())
         self.db = self.client["database_of_monsters"]
         self.collection = self.db["my_monsters"]
 
-    def seed_database(self, amount: int) -> bool:
-        "Seed database with given number of monsters"
+    def seed(self, amount: int) -> bool:
+        """Seed database with given number of monsters"""
         try:
             monsters = []
             for num in range(amount):
@@ -34,14 +36,15 @@ class MonsterDatabase:
             return False
 
     def get_all_monsters(self):
-        return list(self.collection.find())
+        """Returns a list of all monsters in the database, excluding the _id column"""
+        return list(self.collection.find({}, {"_id":0}))
 
-    def get_count(self) -> int:
-        "Return the number of monsters in the collection"
+    def count(self) -> int:
+        """Return the number of monsters in the collection"""
         return self.collection.count_documents({})
 
-    def reset_database(self):
-        "Delete all monsters from the collection"
+    def reset(self):
+        """Delete all monsters from the database"""
         try:
             self.collection.delete_many({})
             return True
@@ -49,8 +52,8 @@ class MonsterDatabase:
             print(f"Error resetting database: {e}")
             return False
 
-    def make_dataframe(self) -> DataFrame:
-        "Makes a dataframe of all monsters in the collection"
+    def dataframe(self) -> DataFrame:
+        """Makes a dataframe of all monsters in the database, excluding the id column"""
         try:
             documents_list = list(self.collection.find({}, {"_id": 0}))
             return pd.DataFrame(documents_list)
@@ -58,8 +61,8 @@ class MonsterDatabase:
             print(f"Error creating dataframe: {e}")
             return DataFrame()
 
-    def make_html_table(self) -> str:
-        "Makes an HTML representation of the dataframe"
+    def html_table(self) -> str:
+        """Makes an HTML representation of the dataframe"""
         try:
             df = self.dataframe()
             if df.empty:
